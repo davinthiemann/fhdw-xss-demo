c94c1209c422d9854fea670fb2cc247a1acd5423
davin.thiemann <davin.thiemann@hdnet.de>
2024-09-04 08:09:18 +0200

    Add changes

--- dombased/dombased.php
+++ dombased/dombased.php
@@ -5,25 +5,38 @@
     <title>DOM-Based XSS Demo</title>
 </head>
 <body>
-<a href="http://localhost:63342/xss-demo/index.html?_ijt=r5n7pshl6hp644afd7vf2uaki9&_ij_reload=RELOAD_ON_SAVE">
-    <button>
-        zur Übersicht
-    </button>
-</a>
-<h1>DOM-Based XSS Test</h1>
-<p>Gib etwas in das Textfeld ein und klicke auf "Eingabe anzeigen".</p>
+<h1>DOM-Based XSS</h1>
+<p>Gib ein Bild ein, das du anzeigen willst: </p>
 
-<input type="text" id="userInput" placeholder="Gib hier etwas ein">
-<button id="displayButton">Eingabe anzeigen</button>
+<input type="text" id="imageUrl" placeholder="Bild-URL eingeben">
+<button id="displayImageButton">Bild anzeigen</button>
 
-<h2>Deine Eingabe:</h2>
-<div id="result"></div>
+<h2>Bild:</h2>
+<div id="imageContainer"></div>
 
 <script>
-    document.getElementById('displayButton').addEventListener('click', function() {
-        var userInput = document.getElementById('userInput').value;
+    function getParameterByName(name) {
+        const url = window.location.href;
+        const nameRegex = name.replace(/[\[\]]/g, '\\$&');
+        const regex = new RegExp('[?&]' + nameRegex + '(=([^&#]*)|&|#|$)');
+        const results = regex.exec(url);
+        return results ? decodeURIComponent(results[2].replace(/\+/g, ' ')) : null;
+    }
 
-        document.getElementById('result').innerHTML = userInput;
+    window.onload = function() {
+        const imageUrl = getParameterByName('image');
+        if (imageUrl) {
+            document.getElementById('imageContainer').innerHTML = '<img src="' + imageUrl + '" alt="Gesuchtes Bild" />';
+        }
+    };
+
+    document.getElementById('displayImageButton').addEventListener('click', function() {
+        const imageUrl = document.getElementById('imageUrl').value;
+
+        const newUrl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?image=' + encodeURIComponent(imageUrl);
+        window.history.pushState({ path: newUrl }, '', newUrl);
+
+        document.getElementById('imageContainer').innerHTML = '<img src="' + imageUrl + '" alt="Gesuchtes Bild" />';
     });
 </script>
 </body>
